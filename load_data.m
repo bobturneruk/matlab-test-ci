dbe3a2f638bcdf6148a48f48f937c9c4d263b65a
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2020-08-05 11:41:04 +0100

    document loader

--- load_data.m
+++ load_data.m
@@ -1,3 +1,26 @@
+% load_data - Load tensile test data
+% Loads tensile test data output by the Squiddly Brand tensile test
+% machine, which is a `.csv` file with columns for `BreakStrain`, 
+% `BreakStress_MPa` , `UltimateStress_MPa` , `Modulus_MPa` and 
+% `YieldStress_MPa`.
+%
+% Syntax:  data = load_data(path)
+%
+% Inputs:
+%    path - Path to data file
+%
+% Outputs:
+%    data - Table containing loaded data
+%
+% Example: 
+%    mydata = load_data('data.csv')
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
 function data = load_data(path)
     opts = detectImportOptions(path); % Initial detection
     opts.VariableNamesLine = 1; % Variable names on line 1
